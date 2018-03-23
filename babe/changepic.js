94531aa2beef06d0cec25e9da290e5d96014609e
BABE <supakorn.wo@ku.th>
2018-03-23 23:39:33 +0700

    babe

---
+++ babe/changepic.js
@@ -0,0 +1,7 @@
+theButton1.onclick = function pictureChange() {
+    document.getElementById('theImage').src = "https://s3-eu-central-1.amazonaws.com/centaur-wp/fundstrategy/prod/content/uploads/2014/11/Powe.jpg";
+}
+
+theButton2.onclick = function pictureChange() {
+    document.getElementById('theImage').src = "http://31.media.tumblr.com/fca646cd8fe87906e605ad7e8d039903/tumblr_mmoz4fWT6U1soh1p8o1_500.png";
+}
